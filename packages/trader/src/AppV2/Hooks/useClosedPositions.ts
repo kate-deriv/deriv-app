7e453d91c231ed7c6b038c5bf3b0e9176f24da1c
kate-deriv <kate@regentmarkets.com>
2024-05-21 15:23:47 +0300

    feat: use hook for real data

--- packages/trader/src/AppV2/Hooks/useClosedPositions.ts
+++ packages/trader/src/AppV2/Hooks/useClosedPositions.ts
@@ -2,11 +2,17 @@ import { WS } from '@deriv/shared';
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { ProfitTable, ProfitTableResponse } from '@deriv/api-types';
 
-type TPros = {
-    contractTypes: string[];
-};
+type TPros =
+    | {
+          date_from?: string;
+          date_to?: string;
+      }
+    | Record<string, never>;
 
-const useClosedPositions = ({ contractTypes }: TPros) => {
+// TODO: we can't filtrate by contractTypes here as for BE High/low and Rise/Fall is ONE contract. So for contractTypes filtration is implemented in position.tsx
+// TODO: refactor this hook for date filtration. e.g. date_from and date_to
+
+const useClosedPositions = ({ date_from, date_to }: TPros) => {
     const [positions, setPositions] = useState<NonNullable<ProfitTable['transactions']>>([]);
     const [isLoading, setLoading] = React.useState(false);
     const positionsRef = useRef<NonNullable<ProfitTable['transactions']>>([]);
@@ -14,19 +20,20 @@ const useClosedPositions = ({ contractTypes }: TPros) => {
     const fetch = useCallback(async () => {
         setLoading(true);
         const data: ProfitTableResponse = await WS.profitTable(50, positionsRef.current.length, {
-            contract_type: contractTypes.length > 0 ? contractTypes : undefined,
+            date_from,
+            date_to,
         });
 
         setLoading(false);
         // TODO: handle errors
         setPositions(prevPositions => [...prevPositions, ...(data?.profit_table?.transactions ?? [])]);
-    }, [contractTypes]);
+    }, [date_from, date_to]);
 
     useEffect(() => {
         setPositions([]);
         positionsRef.current = [];
         fetch();
-    }, [fetch, contractTypes]);
+    }, [fetch, date_from, date_to]);
 
     return { closedPositions: positions, isLoading, fetchMore: fetch };
 };
